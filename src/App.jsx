2e6ddd7cd1e30998bd87d10798c6ca4e0e0dd605
PcMac <pcmac@alexsoto042.duckdns.org>
2024-07-18 15:02:37 -0600

    Nuevo proyecto con React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Nvar from "./components/nvar";
+
+function App() {
+  return (
+    <>
+      <h1>Portafolio</h1>
+      <Nvar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/nvar.jsx
@@ -0,0 +1,22 @@
+import { Camera } from "lucide-react";
+
+function Nvar() {
+  return (
+    <>
+      <ul className="Nvar horizontal no-style">
+        <li>
+          <a href="">inicio</a>
+          <Camera width={"1em"} />
+        </li>
+        <li>
+          <a href="">Tecnologias </a>
+        </li>
+        <li>
+          <a href="">Github </a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Nvar;
