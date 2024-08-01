edd38501e223dfad47761d82e74bf7129e82e16b
Alexsoto042 <alexsoto042a@gmail.com>
2024-08-01 17:37:51 -0600

    Index Modificate

--- src/proyects/Pcolors.jsx
+++ src/proyects/Pcolors.jsx
@@ -9,7 +9,7 @@ function Pcolors() {
         id1={"React"}
         ide2={"Javascript"}
         ide3={"Css"}
-        url={"https://colorspaletes-alexsoto-042.vercel.app"}
+        url={"https://pcolors.netlify.app"}
         descrip={
           "es una aplicación web desarrollada en React que permite a los usuarios crear y guardar combinaciones de colores personalizadas, esta herramienta facilita la selección y la visualización de paletas de colores para diversos proyectos creativos."
         }
